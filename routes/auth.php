ee84e4bfbe4a8353a19b373eafbb3fe29a96e8c2
IKadekNaryasa <qy.naryasa@gmail.com>
2025-11-17 14:04:08 +0800

    memperbaharui routes

--- routes/auth.php
+++ routes/auth.php
@@ -8,10 +8,22 @@ use App\Http\Controllers\Auth\Authenticate;
 use App\Http\Controllers\Auth\ChangePassword;
 use App\Http\Controllers\Auth\ForgotPassword;
 use App\Http\Controllers\Auth\VerificationController;
+use App\Http\Controllers\Operator\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Legalizer\DashboardController as LegalizerDashboardController;
+use App\Http\Middleware\ArsikaAdmin;
+use App\Http\Middleware\ArsikaLegalizer;
+use App\Http\Middleware\ArsikaOperator;
 
 Route::get('/', function () {
     return view('arsika');
 });
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware(ArsikaAuth::class, ArsikaOperator::class);
+Route::get('admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware(ArsikaAuth::class, ArsikaAdmin::class);
+Route::get('legalizer/dashboard', [LegalizerDashboardController::class, 'index'])->name('legalizer.dashboard')->middleware(ArsikaAuth::class, ArsikaLegalizer::class);
+
+
 Route::middleware(ArsikaGuest::class)->group(function () {
     Route::get('/email/verify', function () {
         return view('auth.verify');
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LegalController;
-use App\Http\Controllers\Operator\DashboardController;
-use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
-use App\Http\Controllers\Legalizer\DashboardController as LegalizerDashboardController;
-use App\Http\Middleware\ArsikaAdmin;
-use App\Http\Middleware\ArsikaAuth;
-use App\Http\Middleware\ArsikaLegalizer;
-use App\Http\Middleware\ArsikaOperator;
-
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware(ArsikaAuth::class, ArsikaOperator::class);
-Route::get('admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware(ArsikaAuth::class, ArsikaAdmin::class);
-Route::get('legalizer/dashboard', [LegalizerDashboardController::class, 'index'])->name('legalizer.dashboard')->middleware(ArsikaAuth::class, ArsikaLegalizer::class);
-
 // Arsip
 Route::middleware('web')->group(base_path('routes/arsip.php'));
 // Bidang
 Route::middleware('web')->group(base_path('routes/bidang.php'));
-
 // Authentication
 Route::middleware('web')->group(base_path('routes/auth.php'));
-
 // Authentication
 Route::middleware('web')->group(base_path('routes/user.php'));
