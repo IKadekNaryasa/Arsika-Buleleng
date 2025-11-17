6571d216a3ba7bfefae731f11d115a3d086e0b5f
IKadekNaryasa <qy.naryasa@gmail.com>
2025-11-17 11:45:39 +0800

    merubah canical

--- resources/views/components/operator/layout.blade.php
+++ resources/views/components/operator/layout.blade.php
@@ -11,7 +11,6 @@
     <title>ARSIKA</title>
 
     <meta name="description" content="Page of Operator">
-    <link rel="canonical" href="https://arsika-buleleng.iknproject.site/">
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('ikn_sneat/assets/img/favicon/favicon.ico') }}?v={{ time() }}" />
 
